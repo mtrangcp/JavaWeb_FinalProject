fc409db6c7d1890c24aba6dfeacdcb4b43febce4
mtrangcp <haminhtrang545@gmail.com>
2026-05-07 21:46:08 +0700

    task2

--- src/main/java/com/btvn/projectfinal/config/SecurityConfig.java
+++ src/main/java/com/btvn/projectfinal/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/auth/login")
-                        .loginProcessingUrl("/auth/login")
+                        .loginProcessingUrl("/auth/login-process")
                         .defaultSuccessUrl("/dashboard", true)
                         .failureUrl("/auth/login?error=true")
                         .permitAll()
